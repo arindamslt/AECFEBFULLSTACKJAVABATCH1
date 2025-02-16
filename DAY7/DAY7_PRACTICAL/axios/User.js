8289460763dfa164dce1e7eb76d931b56eb53544
arindamslt <arin.saltlake@gmail.com>
2025-02-16 15:14:49 +0530

    DAY7 PRACTICAL

---
+++ DAY7/DAY7_PRACTICAL/axios/User.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import axios from 'axios'
+import { useState } from 'react'
+function User() {
+    let[user,setUser]=useState([]);
+    const getData=()=>{
+        axios.get("https://randomuser.me/api/?results=20")
+        .then((res)=>{
+            console.log(res.data.results);
+            setUser(res.data.results);
+        })
+        .catch((error)=>{
+            alert("SOMETHING WENT WRONG FETCHING DATA");
+            console.log(error);
+        })
+    }
+  return (
+    <div>
+      <h2>USER DETAILS</h2>
+      <button onClick={getData}>GETDATA</button>
+      {
+         user.length>0 ? 
+         <div style={{width:"30%",margin:"50px auto"}}>
+          <table className='table table-hover table-striped'>
+            <thead className='table-dark'>
+                <tr>
+                <td>ID</td>
+                <td>NAME</td>
+                <td>IMAGE</td>
+                <td>GENDER</td>
+                <td>EMAIL</td>
+                <td>CITY</td>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    user.map((element,index)=>{
+                        return(
+                            <tr>
+                            <td>{index+1}</td>
+                            <td>{element.name.first}</td>
+                            <td><img src={element.picture.medium} height={80} width={80} alt="NO IMAGE"/></td>
+                            <td>{element.gender}</td>
+                            <td>{element.email}</td>
+                            <td>{element.location.city}</td>
+                            </tr>
+                        )
+                    })
+                }
+            </tbody>
+          </table>
+
+         </div>
+         :<h2 style={{color:"red"}}>NO DATA FOUND</h2>
+      }
+    </div>
+  )
+}
+
+export default User
